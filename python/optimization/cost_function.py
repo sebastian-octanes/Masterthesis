039332cde0589bc51ab259ff1a223ea157a5ebe5
Sebastian Weller <weller@like21sux.like.e-technik.uni-erlangen.de>
2018-02-20 14:43:34 +0100

    commit before integrating casadi into the optimization process to see if speedup possible

--- python/optimization/cost_function.py
+++ python/optimization/cost_function.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from matplotlib import pyplot as plt
+from ad import gh
 
 class CostFunction:
     
@@ -47,11 +48,19 @@ class CostFunction:
         n = x.size/6
         cost = 0.0
         for i in range(0,n,1):
-            pos = [x[i*6], x[i*6+1]]
-            #cost = cost + self.track.distance_to_track(pos)
             cost = cost + 0.3 - 0.3 * x[i*6 +2]/34.0 
         return cost
     
+    def cost_dist_track_speed_jac(self):
+        def cost_dist_track_speed(x):
+            n = x.size/6
+            cost = 0.0
+            for i in range(0,n,1):
+                cost = cost + 0.3 - 0.3 * x[i*6 +2]/34.0 
+            return cost
+        
+        gradient, hessian = gh(cost_dist_track_speed)
+        return gradient  
     
     def cost_dist_origin(self, X):
         """Take the vehicle position and compute the distance to the origin"""
--- python/optimization/spielwiese.py
+++ python/optimization/spielwiese.py
@@ -9,40 +9,40 @@ from matplotlib import pyplot as plt
 
 
 
+from casadi import *
+
+
+
+
+
+opti = casadi.Opti()
+x = opti.variable(2)
+#const = lambda x : x[2] + (1-x[0])**2 - x[1]
+
+const = lambda x : x[0]+x[1]-10
+opti.subject_to(const(x) >= 2)
+
+opti.subject_to(x[0] >= 9)
+
+min_f = lambda x: x[0]**2 + x[1]**2
+#min_f = lambda x: x[0]**2 + 100*x[2]**2
+opti.minimize(min_f(x))
+opti.solver('ipopt')
+sol = opti.solve()
+print sol.value(x)
 
 
 #==============================================================================
-# 
-# vehicleBoundPoints = np.empty([6,4, 2])
-# point = np.array([4, 5])
-# print point
-# vehicleBoundPoints[0][0] = point
-# print vehicleBoundPoints
-# 
-# test = np.array([0,1,2,3,4,5,6,7,8,9])
-# print test[-2:]
+# x = SX.sym('x')
+# y = SX.sym('y')
+# z = SX.sym('z')
 # 
 # 
+# nlp = {'x': vertcat(x,y,z), 'f': x**2+100*z**2, 'g': z+(1-x)**2-y }
+# S=nlpsol('S', 'ipopt', nlp)
 # 
-# track = RaceTrack()
-# 
-# tck, u = track.get_spline_tck()
-# ti = np.linspace(0,1, 400)
-# dxdt, dydt = interpolate.splev(ti, tck, der=1)
-# 
-# #track.plot_track_spline()
-# #plt.plot(dxdt, ti)
-# #plt.plot(dydt, ti)
-# #plt.plot(dxdt , ti)
-# #plt.show()
-# 
-# pos = (0.7, 25)
-# print track.distance_to_track_spline(pos)
+# r = S(x0 = [0, 0.0, 0.75], lbg=0, ubg=0)
+# x_opt = r['x']
+# print('x_opt: ', x_opt)
 # 
 #==============================================================================
-p1 = (2, 5)
-p2 = (5, 2)
-x = (2, 2)
-
-d = (x[0]-p1[0]) * (p2[1] - p1[1]) - (x[1] - p1[1])*(p2[0]- p1[0])
-print d
