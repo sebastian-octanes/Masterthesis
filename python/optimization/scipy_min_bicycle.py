f13c494aeeb1670f140f848e3c41eb3b2c1d309e
Sebastian Weller <weller@like21sux.like.e-technik.uni-erlangen.de>
2018-03-07 15:35:44 +0100

    urgh

--- python/optimization/scipy_min_bicycle.py
+++ python/optimization/scipy_min_bicycle.py
@@ -205,27 +205,27 @@ N = 10
 opti = Opti()
 
 all_var = opti.variable(6*(N+1) + N)
-p_tmp = opti.parameter()
-opti.set_value(p_tmp, 0.01)
+#p_tmp = opti.parameter()
+#opti.set_value(p_tmp, 0.01)
 
 #x = opti.variable(6 * (N + 1))
 x = all_var[0:6*(N+1)]
-bet = all_var[6*(N+1):]
+#bet = all_var[6*(N+1):]
 #ineq = [N]
 for k in range(N):
     beta = arctan((lr/(lf +lr)) * tan(x[k * 6 + 5]))
-    bet[k] = beta + arctan((lf + lr) * max_lat_acc *0.25 / x[k*6 +2]**2)        
+    #bet[k] = beta + arctan((lf + lr) * max_lat_acc *0.25 / x[k*6 +2]**2)        
     x[(k+1)*6 + 0] = x[k*6 + 0] + x[k*6 +2] * dt * cos(x[k*6 + 3] + beta)
     x[(k+1)*6 + 1] = x[k*6 + 1] + x[k*6 +2] * dt * sin(x[k*6 + 3] + beta)
     x[(k+1)*6 + 2] = x[k*6 + 2] + x[k*6 +4] * dt 
     x[(k+1)*6 + 3] = x[k*6 +3] + (x[k*6 +2]*dt/lr) * sin(beta)
 
 #bet = arctan((lr/(lf +lr)) * tan(x[0 * 6 + 5])) + arctan((lf + lr) * max_lat_acc *0.25 / fmin(x[0*6 +2]**2, p_tmp))
-bet = 1 / x[0]
+#bet = 1 / x[2]
 #bet = arctan((lf + lr) * max_lat_acc / x[2] )
 #print x[5]    
 #vehicle bounds
-opti.subject_to(bet >= 0.0)
+#opti.subject_to(bet >= 0.0)
 opti.subject_to(opti.bounded(vmin, x[2::6], vmax))
 opti.subject_to(opti.bounded(-9, x[4::6], 9))
 opti.subject_to(opti.bounded(psimin, x[5::6], psimax))
@@ -234,7 +234,7 @@ init_model_state = opti.parameter(4)
 init_m = [2.0, 1.0, 0.1 , math.pi/2, 0,0]
 opti.set_value(init_model_state, init_m[0:4])
 opti.subject_to(x[0:4] == init_model_state[0:4])
-
+opti.set_initial(x[2], 0.1)
 
 #==============================================================================
 # #tangential points constraint
--- python/optimization/spielwiese.py
+++ python/optimization/spielwiese.py
@@ -23,14 +23,6 @@ psimin = -(30.0/180)*math.pi #in rad
 psimax = (30.0/180)*math.pi #in rad
 
 
-
-def cost_dist_(X):
-    cost = 0
-    N = 1
-    for i in range(N):
-        cost = cost = cost + 0.3 - 0.3 * x[i*6 +2]/34.0 
-    return cost
-
 N = 30
 
 #control
@@ -41,13 +33,14 @@ x = MX.sym("x", 4)
 
 beta = arctan((lr/(lf +lr)) * tan(u[1]))
 beta_max = arctan((lf + lr) * max_lat_acc *0.25 / x[2]**2)
+
 ineq1 = beta + beta_max
 ineq2 = -beta + beta_max        
 ineq = vertcat(ineq1, ineq2)
 x_tmp0 = x[0] + x[2] * dt * cos(x[3] + beta) 
 x_tmp1 = x[1] + x[2] * dt * sin(x[3] + beta)
 xdot = vertcat(x_tmp0, x_tmp1) 
-x_tmp2 = x[2] + u[2] * dt 
+x_tmp2 = x[2] + u[0] * dt 
 xdot = vertcat(xdot, x_tmp2)
 x_tmp3 = x[3] + (x[2]*dt/lr) * sin(beta)
 xdot = vertcat(xdot, x_tmp3)
@@ -57,8 +50,10 @@ for i in range(N-1):
     x1 = xdot[i*4 + 1] 
     x2 = xdot[i*4 + 2] 
     x3 = xdot[i*4 + 3] 
-    beta = arctan((lr/(lf +lr)) * tan(u[1 * 2 + 1]))
+    
+    beta = arctan((lr/(lf +lr)) * tan(u[i * 2 + 3]))
     beta_max = arctan((lf + lr) * max_lat_acc *0.25 / x2**2)
+    
     ineq1 = beta + beta_max
     ineq2 = -beta + beta_max
     ineq = vertcat(ineq, ineq1, ineq2)
@@ -66,69 +61,43 @@ for i in range(N-1):
     xdot = vertcat(xdot, x_tmp)
     x_tmp = x1 + x2 * dt * sin(x3 + beta)
     xdot = vertcat(xdot, x_tmp)
-    x_tmp = x2 + u[k * 2] * dt 
+    x_tmp = x2 + u[i * 2 + 2] * dt 
     xdot = vertcat(xdot, x_tmp)
     x_tmp = x3 + (x2*dt/lr) * sin(beta)     
     xdot = vertcat(xdot, x_tmp)    
     
-
-#==============================================================================
-# for k in range(1):
-#     beta = arctan((lr/(lf +lr)) * tan(u[k * 2 + 1]))
-#     beta_max = arctan((lf + lr) * max_lat_acc *0.25 / x[k*6 +2]**2)
-#     ineq1 = beta + beta_max
-#     ineq2 = -beta + beta_max        
-#     x0 = x[k*4 + 0] + x[k*4 +2] * dt * cos(x[k*4 + 3] + beta)  
-#     x1 = x[k*4 + 1] + x[k*4 +2] * dt * sin(x[k*4 + 3] + beta)
-#     x2 = x[k*4 + 2] + u[k*2] * dt 
-#     x3 = x[k*4 +3] + (x[k*4 +2]*dt/lr) * sin(beta)
-#     
-#     ineq = vertcat(ineq, ineq1, ineq2)    
-#     beta = arctan((lr/(lf +lr)) * tan(u[k * 2 + 1]))
-#     beta_max = arctan((lf + lr) * max_lat_acc *0.25 / x[k*6 +2]**2)
-#     ineq1 = beta + beta_max
-#     ineq2 = -beta + beta_max 
-#     x4 = x0 + x2 * dt * cos(x3 + beta)
-#     x5 = x1 + x2 * dt * sin(x3 + beta)
-#     x6 = x2 + u[k * 2] * dt 
-#     x7 = x3 + (x2*dt/lr) * sin(beta)     
-#     
-#     
-# #xdot = vertcat(x0,x1,x2,x3)
-# xdot = vertcat(x0,x1,x2,x3,x4,x5,x6,x7)
-#==============================================================================
-     
+ 
 f = Function('f', [x,u],[xdot, ineq])
 
-#==============================================================================
-# p = opti.parameter(2,4)
-# ar = [[0,1,3,4], [0,1,3,1]]
-# opti.set_value(p[0,:], ar[0])
-# opti.set_value(p[1,:], ar[1])
-# p1 = p[0,0:2]
-# p2 = p[0,2:4]
-# ineq1 = (x[0]-p1[0]) * (p2[1] - p1[1]) - (x[1] - p1[1]) * (p2[0]- p1[0])            
-# 
-# 
-#==============================================================================
 
 
 U = MX.sym("U", (N)*2)
-X0 = MX([0,0,0.1,0,])
+X0 = MX([0,0,0.1,0])
 
-X, INEQ = f(X0, U)
+X, INEQ = f(X0,U)
 X = vertcat(X, INEQ)
 
-J = 0.3 * X[0*4 +2]/34.0 + 0.3 * X[1*4 +2]/34.0
-G = X[0:N*6]
+J =  1/X[2]
+for i in range(1,N,1):
+    J =  J + 1/X[i*4 + 2]
+
+parameter = [0,1,3,4]
+
+p1 = parameter[0:2]
+p2 = parameter[2:4]
+ineq1 = (X[0]-p1[0]) * (p2[1] - p1[1]) - (X[1] - p1[1]) * (p2[0]- p1[0])            
+
+X = vertcat(X, ineq1)
+
+
+G = X[0:N*6 +1]
 
 
 nlp = {'x':U, 'f':J, 'g': G}
 
 # Allocate an NLP solver
-opts = {"ipopt.tol":1e-10, "ipopt.print_level": 4}
+opts = {"ipopt.tol":1e-10, "ipopt.print_level": 1}
 solver = nlpsol("solver", "ipopt", nlp, opts)
-
 
 
 # Bounds on u and initial condition
@@ -138,11 +107,18 @@ lbx = np.array([])
 ubx = np.array([])
 
 for i in range(N):
-    lbg = np.append(lbg, np.array([-inf, -inf, vmin, -inf, 0, 0]))
-    ubg = np.append(ubg, np.array([inf, inf, vmax, inf, inf, inf]))
+    lbg = np.append(lbg, np.array([-inf, -inf, vmin, -inf]))
+    ubg = np.append(ubg, np.array([inf, inf, vmax, inf]))
     lbx = np.append(lbx, np.array([-9, psimin]))
     ubx = np.append(ubx, np.array([ 9, psimax]))
-        
+
+for i in range(N):
+    lbg = np.append(lbg, np.array([0,0]))
+    ubg = np.append(ubg, np.array([inf,inf]))
+
+lbg = np.append(lbg, np.array([0]))
+ubg = np.append(ubg, np.array([inf]))
+       
 
 arg = {}
 arg["lbx"] = lbx
@@ -152,15 +128,8 @@ arg["ubx"] = ubx
 arg["lbg"] = lbg
 arg["ubg"] = ubg
 
+
 # Solve the problem
 res = solver(**arg)
+print(res["x"])
 
-#==============================================================================
-# 
-
-# arg["x0"] =   0.4
-# 
-# # Bounds on g
-# arg["lbg"] = [10,0,0]
-# arg["ubg"] = [10,0,2]
-#==============================================================================
