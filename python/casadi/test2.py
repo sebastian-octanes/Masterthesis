2fb871d2a8bd26ead8a63814a340a9fa34a75314
Sebastian <sebastian.weller@octanes.de>
2018-03-09 09:28:31 +0100

    modified test2

--- python/casadi/test2.py
+++ python/casadi/test2.py
@@ -9,14 +9,14 @@ from casadi import *
 from numpy import *
 
 dt = 0.1
-N = 3
+N = 4
 lf = 0.9 
 lr = 0.640  
 lb = 1.440    
 dt = 0.05
 max_lat_acc = 20
 vmax = 60 # m/s
-vmin = 0  # no driving backwards
+vmin = 0.001  # no driving backwards
 psimin = -(30.0/180)*math.pi #in rad
 psimax = (30.0/180)*math.pi #in rad
 
@@ -25,18 +25,16 @@ x = SX.sym("x",(N+1) *6)
 G = vertcat(x[0], x[1], x[2], x[3])
 
 for i in range (N):
+
     beta = arctan((lr/(lf +lr)) * tan(x[i*6 + 5]))
-    max_beta =  arctan(1.0/2 * (lf + lr) * max_lat_acc / G[2]**2)    
+    max_beta =  arctan(1.0/2 * (lf + lr) * max_lat_acc / x[(i*6) +2]**2)    
     ineq1 = -beta + max_beta
     ineq2 =  beta + max_beta
-    G = vertcat(G, G[0] - G[2] * dt * cos(G[3] + beta))
-    G = vertcat(G, G[1] - G[2] * dt * sin(G[3] + beta))
-    G = vertcat(G, G[2] - x[i*6 + 4] * dt)
-    G = vertcat(G, G[3] - G[2] * dt /lr * sin(beta))
+    G = vertcat(G, x[(i+1)*6   ] - x[i*6   ] + x[(i)*6 +2] * dt * cos(x[i*6 +3] + beta))
+    G = vertcat(G, x[(i+1)*6 +1] - x[i*6 +1] + x[(i)*6 +2] * dt * sin(x[i*6 +3] + beta))
+    G = vertcat(G, x[(i+1)*6 +2] - x[i*6 +2] + x[(i)*6 +4] * dt)
+    G = vertcat(G, x[(i+1)*6 +3] - x[i*6 +3] + x[(i)*6 +2] * dt /lr * sin(beta))
     G = vertcat(G, ineq1, ineq2)
-
-print G
-
  
 #==============================================================================
 # = x[1] + x[2] * dt * sin(x[3] + beta)
@@ -46,12 +44,13 @@ print G
 # = x[3] + (x[2]*dt/lr) * sin(beta)
 # 
 #==============================================================================
-J = 1/x[8] + 1/x[14]
-#==============================================================================
-# for i in range(N):
-#     J = J +  x[i*6 + 2]
-# 
-#==============================================================================
+
+
+J = 1/x[2]
+for i in range(N):
+    print i
+    J = J +  1/x[(i+1)*6 + 2]
+
 
 nlp = {'x':x, 'f':J, 'g': G}
 
@@ -64,14 +63,14 @@ solver = nlpsol("solver", "ipopt", nlp, opts)
 lbg = np.array([0,0,0.01,0])
 ubg = np.array([0,0,0.01,0])
 
-lbx = np.array([-inf, -inf, vmin, -inf, -9, -inf])
-ubx = np.array([ inf,  inf, vmax,  inf,  9,  inf])
+lbx = np.array([-inf, -inf, vmin, -inf, -9, -psimin])
+ubx = np.array([ inf,  inf, vmax,  inf,  9,  psimax])
 
 x0 =  np.array([0,0,0.01,0, 0 ,0])
 for i in range(N):
-    x0  = np.append(x0,  np.array([   0,    0, 0.01,    0,   0,   0]))
-    lbg = np.append(lbg, np.array([-inf, -inf, vmin, -inf,   0,   0]))
-    ubg = np.append(ubg, np.array([ inf,  inf, vmax,  inf, inf, inf]))
+    x0  = np.append(x0,  np.array([0, 0, 0.01, 0,   0,   0]))
+    lbg = np.append(lbg, np.array([0, 0, 0, 0,   0,   0]))
+    ubg = np.append(ubg, np.array([0, 0, 0, 0, inf, inf]))
     lbx = np.append(lbx, np.array([-inf, -inf, vmin, -inf,  -9, psimin]))
     ubx = np.append(ubx, np.array([ inf,  inf, vmax,  inf,   9, psimax]))
 
@@ -87,3 +86,35 @@ arg["x0" ] = x0
 
 res = solver(**arg)
 print res["x"]
+
+#==============================================================================
+# 
+# print res["x"][0]
+# 
+# tmp = np.arange(res["x"].size()[0])
+# for i in range(res["x"].size()[0]):
+#     print i
+#     #tmp[i] = round(res["x"][i], 6)   
+#     #tmp[i] = float("{0:.2f}".format(res["x"][i]))
+#     tmp[i] = res["x"][i]
+# 
+# for i in range(0,res["x"].size()[0] /6, 1):
+#     print tmp[i*6 : i*6 + 6]
+# 
+#==============================================================================
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
