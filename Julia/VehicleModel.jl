05051c796660d7d7c1bc75f102d0e30b197e0aa9
Sebastian Weller <weller@like21sux.like.e-technik.uni-erlangen.de>
2018-03-20 20:01:21 +0100

    morgen sollte ich die simulation zum laufen bringen können. hoffentlich ist es dann noch schnell genug. aktuell schaut es nicht so aus

---
+++ Julia/VehicleModel.jl
@@ -0,0 +1,71 @@
+module VehicleModel
+
+lf = 0.9    # distance tire to COG
+lr = 0.640
+lb = 1.440    #width of car
+beta = 0.0    #slip angle
+gammaf = 0.0  #steering angle
+A     = 2.5    #vehicle cross section
+FEngine = 4000   # 5000N for car
+mass    = 170   #kg
+Cd      = 0.5   #drag coefficient
+p       = 1.225  # air desity in kg/m^3
+A       = 2.0    #vehicle cross section
+Crr     = 0.014  #roll resistance coefficient
+max_speed = 120/3.6 # 120km/h /3.6 = m/s
+max_long_acc = 10   #m/s**2 longitudinal acceleration max
+max_long_dec = 10   #m/s**2 longitudinal deceleration max
+max_lat_acc = 20  # 2g lateral acceleration
+max_steering_angle = (30.0/180.0)*pi #
+max_acceleration_time = 4.0 #seconds
+
+struct CarPose
+    x::Float64
+    y::Float64
+    v::Float64
+    yaw::Float64
+end
+struct CarControls
+    acc::Float64
+    steer::Float64
+end
+struct CarState
+    x::Float64
+    y::Float64
+    v::Float64
+    yaw::Float64
+    acc::Float64
+    steer::Float64
+    dt::Float64
+end
+
+function computeTimeStep(carPose, carControl, dt)
+    beta = atan((lr/(lf + lr)) * tan(carControl.steer))
+    max_beta =  atan(1.0/2 * (lf + lr) * max_lat_acc / carPose.v^2)
+    if (beta >= 0)
+        beta = min(beta, max_beta)
+    else
+        beta = - min(-beta, max_beta)
+    end
+    x_1 = carPose.x + carPose.v * dt * cos(carPose.yaw + beta)
+    y_1 = carPose.y + carPose.v * dt * sin(carPose.yaw + beta)
+    yaw_1 = carPose.yaw + (carPose.v * dt/lr) * sin(beta)
+    v_1 = carPose.v + carControl.acc * dt
+    v_1 = (v_1 > max_speed) ?  max_speed : v_1
+    CarPose(x_1, y_1, v_1, yaw_1)
+end
+
+export CarState, computeTimeStep, CarPose, CarControls, max_steering_angle, max_long_acc, max_long_dec
+
+end
+
+#=
+using VehicleModel
+
+carState = VehicleModel.CarState(0,0,0.1,pi,0,0,0.1)
+carPose = VehicleModel.CarPose(0,0,0,0)
+carControl = VehicleModel.CarControls(1, 0)
+carPose = VehicleModel.computeTimeStep(carPose, carControl, 0.1)
+pose = VehicleModel.computeTimeStep(carPose, carControl, 0.1)
+println(pose)
+=#
---
+++ Julia/raceCourse.jl
@@ -0,0 +1,86 @@
+module RaceCourse
+
+using Interpolations
+using PyPlot
+
+
+function computeGradientPoints(itpBound, evalPoint)
+ x1, y1  = itpBound[evalPoint + 0.01,1], itpBound[evalPoint + 0.01,2]
+ x2, y2  = itpBound[evalPoint - 0.01,1], itpBound[evalPoint - 0.01,2]
+ return [x1, y1, x2, y2]
+end
+
+function computeGradientAngle(itpBound, evalPoint)
+ x1, y1  = itpBound[evalPoint - 0.01,1], itpBound[evalPoint - 0.01,2]
+ x2, y2  = itpBound[evalPoint + 0.01,1], itpBound[evalPoint + 0.01,2]
+ x3, y3  = x1 + 1, y1
+ -1* (atan2(y3 - y1, x3 - x1)- atan2(y2 -y1, x2 -x1))
+end
+
+function getSplinePosition(itpBound, x, y)
+ t = 0:0.001:1
+ smallest = 1000
+ indx = 0
+ for i = 0:0.001:1
+  x1, y1  = itpBound[i,1], itpBound[i,2]
+  dist = sqrt((x1-x)^2 + (y - y1)^2)
+  if dist < smallest
+   smallest = dist
+   indx = i
+  end
+ end
+ return indx
+end
+
+function buildRaceTrack(radius, trackWidth, OriginX, OriginY)
+
+
+ t = 0:.1:1
+ x = (radius + trackWidth/2) * sin.(2π*t) + OriginX
+ y = (radius + trackWidth/2) * cos.(2π*t) + OriginY
+ aOutBound = hcat(x,y)
+
+ x = (radius - trackWidth/2) * sin.(2π*t) + OriginX
+ y = (radius - trackWidth/2) * cos.(2π*t) + OriginY
+ aInBound = hcat(x,y)
+
+ x = radius * sin.(2π*t) + OriginX
+ y = radius * cos.(2π*t) + OriginY
+ aTrack = hcat(x,y)
+
+ itpTrack = scale(interpolate(aTrack, (BSpline(Cubic(Natural())), NoInterp()), OnGrid()), t, 1:2)
+ itpOutBound = scale(interpolate(aOutBound, (BSpline(Cubic(Natural())), NoInterp()), OnGrid()), t, 1:2)
+ itpInBound = scale(interpolate(aInBound, (BSpline(Cubic(Natural())), NoInterp()), OnGrid()), t, 1:2)
+ return itpTrack, itpOutBound, itpInBound
+end
+
+function plotRaceTrack(itpTrack, itpOutBound, itpInBound)
+
+ tfine = 0:.01:1
+
+ xsT, ysT = [itpTrack[t,1] for t in tfine], [itpTrack[t,2] for t in tfine]
+ xsO, ysO = [itpOutBound[t,1] for t in tfine], [itpOutBound[t,2] for t in tfine]
+ xsI, ysI = [itpInBound[t,1] for t in tfine], [itpInBound[t,2] for t in tfine]
+
+ plot(xsT, ysT, label="spline")
+ plot(xsO, ysO, label="spline")
+ plot(xsI, ysI, label="spline")
+end
+
+export plotRaceTrack, buildRaceTrack, computeGradientPoints, computeGradientAngle, getSplinePosition
+
+end
+
+#=
+using RaceCourse
+using PyPlot
+
+itpTrack, itpOutBound, itpInBound = buildRaceTrack(100, 50)
+plotRaceTrack(itpTrack, itpOutBound, itpInBound)
+x = computeGradientPoints(itpTrack, 0.1)
+alpha = computeGradientAngle(itpTrack, 0.1)
+println(alpha)
+pos = getSplinePosition(itpTrack, 100, 10)
+println(pos)
+plot(x[1:2:4], x[2:2:4])
+=#
